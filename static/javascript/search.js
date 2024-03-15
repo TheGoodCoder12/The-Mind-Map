5ed6da52dbfbfa416fc5babdb969d6a4e00f4ecb
Lalit Machra <lalitmachra0403@gmail.com>
2024-03-15 18:59:12 +0530

    Trying to fix bugs in search

--- static/javascript/search.js
+++ static/javascript/search.js
@@ -1,22 +1,24 @@
-const searchType = document.getElementById('searchType')
-const dateChosen = document.getElementById('dateChosen')
-const peopleChosen = document.getElementById('peopleChosen')
-const cluesChosen = document.getElementById('cluesChosen')
+document.addEventListener('DOMContentLoaded', function() {
+  const searchType = document.getElementById('searchType')
+  const dateChosen = document.getElementById('dateChosen')
+  const peopleChosen = document.getElementById('peopleChosen')
+  const cluesChosen = document.getElementById('cluesChosen')
 
-searchType.addEventListener('change', function() {
-  if (this.value == 'date') {
-    dateChosen.classList.remove('hidden');
-    peopleChosen.classList.add('hidden');
-    cluesChosen.classList.add('hidden');
-  }
-  else if (this.value == 'people') {
-    dateChosen.classList.add('hidden');
-    peopleChosen.classList.remove('hidden');
-    cluesChosen.classList.add('hidden');
-  }
-  else if (this.value == 'clues') {
-    dateChosen.classList.add('hidden');
-    peopleChosen.classList.add('hidden');
-    cluesChosen.classList.remove('hidden');
-  }
+  searchType.addEventListener('change', function() {
+    if (this.value == 'date') {
+      dateChosen.classList.remove('hidden');
+      peopleChosen.classList.add('hidden');
+      cluesChosen.classList.add('hidden');
+    }
+    else if (this.value == 'people') {
+      dateChosen.classList.add('hidden');
+      peopleChosen.classList.remove('hidden');
+      cluesChosen.classList.add('hidden');
+    }
+    else if (this.value == 'clues') {
+      dateChosen.classList.add('hidden');
+      peopleChosen.classList.add('hidden');
+      cluesChosen.classList.remove('hidden');
+    }
+  });
 });
