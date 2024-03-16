f9c582acad606d96b16d9bd34e9c938d946c7fd1
Lalit Machra <lalitmachra0403@gmail.com>
2024-03-17 00:00:26 +0530

    Improved search page

--- static/javascript/search.js
+++ static/javascript/search.js
@@ -1,24 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const searchType = document.getElementById('searchType')
   const dateChosen = document.getElementById('dateChosen')
   const peopleChosen = document.getElementById('peopleChosen')
   const cluesChosen = document.getElementById('cluesChosen')
 
-  searchType.addEventListener('change', function() {
+  searchType.addEventListener('change', function () {
     if (this.value == 'date') {
       dateChosen.classList.remove('hidden');
       peopleChosen.classList.add('hidden');
       cluesChosen.classList.add('hidden');
+      document.getElementById('date').required = true;
     }
     else if (this.value == 'people') {
       dateChosen.classList.add('hidden');
       peopleChosen.classList.remove('hidden');
       cluesChosen.classList.add('hidden');
+      document.getElementById('people').required = true;
     }
     else if (this.value == 'clues') {
       dateChosen.classList.add('hidden');
       peopleChosen.classList.add('hidden');
       cluesChosen.classList.remove('hidden');
+      document.getElementById('clues').required = true;
     }
   });
 });
